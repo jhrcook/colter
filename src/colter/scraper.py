faa10766df41706d6e97b10137e8b80f95034be9
Josh Cook <joshuacook0023@gmail.com>
2025-02-27 14:00:37 -0500

    chore: remove unused code

--- src/colter/scraper.py
+++ src/colter/scraper.py
@@ -1,28 +1,10 @@
 """Web scraping module."""
 
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from loguru import logger
 
 BASEURL = "https://colterbackcountry.com/products/thorofare-net"
-
-
-def _extract_price_badge_data(soup: BeautifulSoup) -> dict[str, list[str]] | None:
-    price = soup.find(class_="price")
-
-    if price is None:
-        logger.error("Failed to find price.")
-        return None
-
-    if not isinstance(price, Tag):
-        logger.error(f"Price is a '{type(price)}' not a 'Tag'.")
-        return None
-
-    badges = {}
-    for badge in price.findAll(class_="badge"):
-        badges[badge.text.strip()] = badge.attrs["class"]
-
-    return badges
 
 
 def is_thorofare_net_soldout() -> bool:
